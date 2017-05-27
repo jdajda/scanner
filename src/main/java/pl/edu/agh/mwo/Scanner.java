acc93ba9eb8ba688edbedf19fbdc4f49dcde28db
nmateusz <mateusznoszczynski@wp.pl>
2017-05-27 16:45:05 +0200

    zmiana nazw

--- src/main/java/pl/edu/agh/mwo/Scanner.java
+++ src/main/java/pl/edu/agh/mwo/Scanner.java
@@ -4,35 +4,38 @@ public class Scanner {
 
 	String text;
 
-	public Scanner(String init) {
-		text = init;
+	public Scanner(String initText) {
+		text = initText;
 	}
 
 	public String get() {
-		String result = "";
-		boolean ok = false;
-		while (!ok) {
+		String resultText = "";
+		boolean functionReturnValue = false;
+		while (!functionReturnValue) {
 			if (text.length() == 0) {
-				return result;
-			} else if (text.substring(0, 1).matches("[ \t\n]")) {
+				return resultText;
+			} 
+			else if (text.substring(0, 1).matches("[ \t\n]")) {
 				text = text.substring(1);
-			} else if (text.substring(0, 1).matches("%")) {
+			} 
+			else if (text.substring(0, 1).matches("%")) {
 				while (!text.substring(0, 1).matches("\n")) {
 					text = text.substring(1);
 				}
-			} else {
-				ok = true;
+			} 
+			else {
+				functionReturnValue = true;
 			}
 		}
 
 		if (text.length() > 0 && text.substring(0, 1).matches("[()]")) {
-			result = text.substring(0, 1);
+			resultText = text.substring(0, 1);
 			text = text.substring(1);
 		}
 		while (text.length() > 0 && !text.substring(0, 1).matches("[ \t\n%()]")) {
-			result += text.substring(0, 1);
+			resultText += text.substring(0, 1);
 			text = text.substring(1);
 		}
-		return result.toLowerCase();
+		return resultText.toLowerCase();
 	}
 }
